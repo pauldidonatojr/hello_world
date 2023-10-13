742726035daa853ee518d464bf0f70cc3ab9a290
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2023-10-14 02:23:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // app.js
 console.log("Hello, World!");
+app.use("/", (req, res) => {
+  console.log('request received');
+  // Create a JSON response
+  const jsonResponse = { message: 'Hello, this is a JSON response!' };
+  
+  // Send the JSON response to the client
+  res.json(jsonResponse);
+});
